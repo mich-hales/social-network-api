68228c770772e662e018b74c1b0819c0e5aea970
mich-hales <michellehales93@gmail.com>
2022-11-18 18:11:58 -0700

    added framework to controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,32 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // get all thoughts
+    getThoughts(req, res) {
+
+    },
+    // create new thought
+    createThought(req, res) {
+
+    },
+    // get single thought
+    getSingleThought(req, res) {
+
+    },
+    // update single thought
+    updateThought(req, res) {
+
+    },
+    // delete single thought
+    deleteThought(req, res) {
+
+    },
+    // create reaction and attach to thought
+    createReaction(req, res) {
+
+    },
+    // delete reaction
+    deleteReaction(req, res) {
+
+    },
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,16 +2,33 @@ const { User } = require('../models');
 
 module.exports = {
     // get all users
-
+    getUsers(req, res) {
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    },
     // create user
-    
-    // get single user
+    createUser(req, res) {
 
+    },
+    // get single user
+    getSingleUser(req, res) {
+
+    },
     // update a single user
-    
+    updateUser(req, res) {
+
+    },
     // delete a single user
-    
+    deleteUser(req, res) {
+
+    },
     // add a user to friend list
-    
+    addFriend(req, res) {
+
+    },
     // delete a user from friend list
-}
+    deleteFriend(req, res) {
+
+    }
+};
