e5e8451d8249bf80347f6e6a8a8f1739d8117ef3
mich-hales <michellehales93@gmail.com>
2022-11-18 18:01:07 -0700

    added thoughtRoutes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,17 @@
 const { User } = require('../models');
 
 module.exports = {
+    // get all users
+
+    // create user
     
+    // get single user
+
+    // update a single user
+    
+    // delete a single user
+    
+    // add a user to friend list
+    
+    // delete a user from friend list
 }
