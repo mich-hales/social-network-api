5fe383409297f04b7605bdd03d2b0cf1851d7dcb
mich-hales <michellehales93@gmail.com>
2022-11-18 18:23:11 -0700

    updates to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,19 +9,53 @@ module.exports = {
     },
     // create user
     createUser(req, res) {
-
+        User.create(req.body)
+        .then(user(user => res.json(user)))
+        .catch((err) => res.status(500).json(err));
     },
     // get single user
     getSingleUser(req, res) {
-
+        User.findOne({ _id: req.params.userId })
+        .then((user) => {
+            if(!user) {
+                res.status(404).json({message: 'No user found with this ID'})
+            } else {
+                res.json(user)
+            };
+        })
+        .catch((err) => res.status(500).json(err));
     },
     // update a single user
     updateUser(req, res) {
-
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body},
+            { runValidators: true, new: true})
+            .then((user) => {
+            if(!user) {
+                res.status(404).json({ message: 'No user with this id found'})
+            } else {
+                res.json(user)
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
     },
     // delete a single user
     deleteUser(req, res) {
-
+        User.findOneAndDelete(
+            { _id: req.params.userId })
+            .then((user) => {
+                if(!user) {
+                    res.status(404).json({ message: 'No user found with this id'})
+                } else {
+                    res.json(user)
+                }
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
     },
     // add a user to friend list
     addFriend(req, res) {
