be4630f05def61674d6d283570dc041925d7ac9c
mich-hales <michellehales93@gmail.com>
2022-11-18 17:55:34 -0700

    added userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,22 @@
 const router = require('express').Router();
+
+const {
+    getUsers,
+    createUser,
+    getSingleUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController');
+
+// api/users
+router.route('/').get(getUsers).post(createUser);
+
+// api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
